1ac54fe992ef60354393f394e2785f013553a509
november295536 <peter840606@gmail.com>
2022-03-01 01:27:50 +0800

    Fix returning null value error at q_remove_tail

--- queue.c
+++ queue.c
@@ -135,7 +135,7 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
     element_t *node = list_last_entry(head, element_t, list);
     list_del(&node->list);
     cpynstr(sp, node->value, bufsize);
-    return NULL;
+    return node;
 }
 
 /*
